a086564ef86e3b60bc9181a1394493515d9987cf
fanuir <fanuir@gmail.com>
2015-07-31 13:27:32 -0400

    Better details pop up formatting

--- app/src/main/java/com/fanuir/octavoreader/StoryInfoDialogFragment.java
+++ app/src/main/java/com/fanuir/octavoreader/StoryInfoDialogFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
@@ -12,6 +13,10 @@ import android.widget.TextView;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import org.json.JSONArray;
+
+import java.text.NumberFormat;
 
 /**
  * Created by ivy on 7/29/15.
@@ -34,14 +39,16 @@ public class StoryInfoDialogFragment extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
         JsonParser jsonParser = new JsonParser();
-        JsonObject story = (JsonObject) jsonParser.parse(getArguments().getString("story"));
+        final JsonObject story = (JsonObject) jsonParser.parse(getArguments().getString("story"));
 
         builder.setTitle(story.get("title").getAsString())
                 .setView(inflater.inflate(R.layout.dialog_story_info, null))
                 .setPositiveButton(R.string.dialog_story_read, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        System.out.println("Read story.");
+                        Intent intent = ReaderActivity.newInstance(getActivity());
+                        intent.putExtra("id", story.get("id").getAsString());
+                        startActivity(intent);
                     }
                 })
                 .setNeutralButton(R.string.dialog_story_actions, new DialogInterface.OnClickListener() {
@@ -69,14 +76,70 @@ public class StoryInfoDialogFragment extends DialogFragment {
         Dialog dialog = getDialog();
 
         TextView detailsView = (TextView) dialog.findViewById(R.id.dialog_story_info);
-        String details = String.format("<p><b>Authors: </b>%s</p>", LibraryUtils.printJsonArray(story.get("authors").getAsJsonArray()));
-        details += String.format("<p><b>Fandoms: </b>%s</p>", LibraryUtils.printJsonArray(story.get("fandoms").getAsJsonArray()));
-        details += String.format("<p><b>Category: </b>%s</p>", LibraryUtils.printJsonArray(story.get("categories").getAsJsonArray()));
-        details += String.format("<p><b>Relationships: </b>%s</p>", LibraryUtils.printJsonArray(story.get("relationships").getAsJsonArray()));
-        details += String.format("<p><b>Characters: </b>%s</p>", LibraryUtils.printJsonArray(story.get("characters").getAsJsonArray()));
-        details += String.format("<p><b>Tags: </b>%s</p>", LibraryUtils.printJsonArray(story.get("tags").getAsJsonArray()));
-        details += String.format("<p><b>Summary: </b>%s</p>", story.get("summary").getAsString());
+        String details = "";
+        String[] tags = {"authors", "fandoms", "categories", "relationships", "characters", "tags"};
+        String[] properties = {"summary"};
+        String[] stats = {"avail_chapters", "word_count", "kudos", "hits", "bookmarks"};
+
+        for (String tag : tags) {
+            details += safeAddJsonArray(story, tag);
+        }
+        for (String property : properties){
+            details += safeAddJsonString(story, property);
+        }
+        for (String stat : stats){
+            details += safeAddJsonInt(story, stat);
+        }
+
         detailsView.setText(Html.fromHtml(details));
+    }
+
+    public String safeAddJsonArray(JsonObject story, String property) {
+        String list = LibraryUtils.printJsonArray(story.get(property).getAsJsonArray());
+        if (!list.equals("")) {
+            String desc = property.substring(0, 1).toUpperCase() + property.substring(1);
+            return String.format("<p><b>%s: </b>%s</p>", desc ,list);
+        } else {
+            return "";
+        }
+    }
+
+    public String safeAddJsonString(JsonObject story, String property) {
+        String item = story.get(property).getAsString();
+        if (!item.equals("")) {
+            String desc;
+            if(property.equals("avail_chapters")){
+                desc = "Chapters";
+                String total = (story.get("total_chapters").getAsInt() == -1) ? "?" : story.get("total_chapters").getAsString();
+                item = String.format("%s/%s", item, total);
+            } else if(property.equals("word_count")) {
+                desc = "Word Count";
+            } else {
+                desc = property.substring(0, 1).toUpperCase() + property.substring(1);
+            }
+            return String.format("<p><b>%s: </b>%s</p>", desc, item);
+        } else {
+            return "";
+        }
+    }
+
+    public String safeAddJsonInt(JsonObject story, String property){
+        String item = NumberFormat.getIntegerInstance().format(story.get(property).getAsInt());
+        if (!item.equals("")) {
+            String desc;
+            if(property.equals("avail_chapters")){
+                desc = "Chapters";
+                String total = (story.get("total_chapters").getAsInt() == -1) ? "?" : story.get("total_chapters").getAsString();
+                item = String.format("%s/%s", item, total);
+            } else if(property.equals("word_count")) {
+                desc = "Word Count";
+            } else {
+                desc = property.substring(0, 1).toUpperCase() + property.substring(1);
+            }
+            return String.format("<p><b>%s: </b>%s</p>", desc, item);
+        } else {
+            return "";
+        }
     }
 
 }
